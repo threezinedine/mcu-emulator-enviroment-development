c705d670890ac579ef0f6731facb7c895b3ca870
threezinedine <threezinedine@gmail.com>
2025-11-30 11:07:50 +0700

    [feature] setup debug layer

--- engine/examples/render.c
+++ engine/examples/render.c
@@ -5,6 +5,16 @@ int main(void)
 	meedPlatformMemoryInitialize();
 	meedRenderInitialize();
 
+	struct MEEDWindowData* pWindow = meedWindowCreate(1600, 1400, "MEEDEngine Vulkan Renderer Example");
+
+	while (!pWindow->shouldClose)
+	{
+		meedWindowPollEvents(pWindow);
+		// Rendering and update logic would go here
+	}
+
+	meedWindowDestroy(pWindow);
+
 	meedRenderShutdown();
 	meedPlatformMemoryShutdown();
 	return 0;
--- engine/src/modules/render/renderer_vulkan.c
+++ engine/src/modules/render/renderer_vulkan.c
@@ -13,13 +13,18 @@ static const char* instanceExtensions[] = {
 };
 
 static const char* layers[] = {
+#if MEED_DEBUG
 	"VK_LAYER_KHRONOS_validation",
+#endif
 };
 
 /// @brief The context of the Vulkan renderer.
 struct MEEDVulkan
 {
 	VkInstance instance;
+#if MEED_DEBUG
+	VkDebugUtilsMessengerEXT debugMessenger;
+#endif
 };
 
 struct MEEDVulkan* g_vulkan = MEED_NULL; // Global Vulkan instance
@@ -27,6 +32,11 @@ struct MEEDVulkan* g_vulkan = MEED_NULL; // Global Vulkan instance
 static struct MEEDReleaseStack* s_releaseStack = MEED_NULL; // Global release stack instance
 
 static void createVulkanInstance();
+#if MEED_DEBUG
+static void createValidationLayers();
+#endif
+
+static void deleteGlobalVulkanInstance(void*);
 
 void meedRenderInitialize()
 {
@@ -38,10 +48,23 @@ void meedRenderInitialize()
 	s_releaseStack = meedReleaseStackCreate();
 
 	g_vulkan = MEED_MALLOC(struct MEEDVulkan);
+	meedReleaseStackPush(s_releaseStack, MEED_NULL, deleteGlobalVulkanInstance);
 
 	createVulkanInstance();
+#if MEED_DEBUG
+	createValidationLayers();
+#endif
 
 	s_isInitialized = MEED_TRUE;
+}
+
+static void deleteGlobalVulkanInstance(void* pData)
+{
+	MEED_UNUSED(pData);
+
+	MEED_ASSERT(g_vulkan != MEED_NULL);
+
+	MEED_FREE(g_vulkan, struct MEEDVulkan);
 }
 
 void meedRenderShutdown()
@@ -50,8 +73,6 @@ void meedRenderShutdown()
 	MEED_ASSERT(g_vulkan != MEED_NULL);
 	MEED_ASSERT(s_releaseStack != MEED_NULL);
 	// Implementation of rendering module shutdown
-
-	MEED_FREE(g_vulkan, struct MEEDVulkan);
 
 	meedReleaseStackDestroy(s_releaseStack);
 	meedWindowShutdown();
@@ -73,42 +94,16 @@ static void createVulkanInstance()
 	appInfo.engineVersion	   = VK_MAKE_VERSION(1, 0, 0);
 	appInfo.apiVersion		   = VK_API_VERSION_1_3;
 
-	VkInstanceCreateInfo createInfo	   = {};
-	createInfo.sType				   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	createInfo.pApplicationInfo		   = &appInfo;
-	createInfo.enabledExtensionCount   = MEED_ARRAY_SIZE(instanceExtensions);
-	createInfo.ppEnabledExtensionNames = instanceExtensions;
-	createInfo.enabledLayerCount	   = MEED_ARRAY_SIZE(layers);
-	createInfo.ppEnabledLayerNames	   = layers;
+	VkInstanceCreateInfo instanceCreateInfo	   = {};
+	instanceCreateInfo.sType				   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceCreateInfo.pApplicationInfo		   = &appInfo;
+	instanceCreateInfo.enabledExtensionCount   = MEED_ARRAY_SIZE(instanceExtensions);
+	instanceCreateInfo.ppEnabledExtensionNames = instanceExtensions;
+	instanceCreateInfo.enabledLayerCount	   = MEED_ARRAY_SIZE(layers);
+	instanceCreateInfo.ppEnabledLayerNames	   = layers;
 
-#if 0
-	VK_ASSERT(vkCreateInstance(&createInfo, MEED_NULL, &g_vulkan->instance));
+	VK_ASSERT(vkCreateInstance(&instanceCreateInfo, MEED_NULL, &g_vulkan->instance));
 	meedReleaseStackPush(s_releaseStack, MEED_NULL, deleteVulkanInstance);
-#else
-	VkResult result = vkCreateInstance(&createInfo, MEED_NULL, &g_vulkan->instance);
-	if (result != VK_SUCCESS)
-	{
-		struct MEEDPlatformConsoleConfig config = {};
-		config.color							= MEED_CONSOLE_COLOR_RED;
-		meedPlatformSetConsoleConfig(config);
-
-		if (result == VK_ERROR_INCOMPATIBLE_DRIVER)
-		{
-			meedPlatformPrint("Cannot find a compatible Vulkan ICD (Install the latest drivers from your GPU vendor).");
-		}
-		else if (result == VK_ERROR_EXTENSION_NOT_PRESENT)
-		{
-			meedPlatformPrint(
-				"Cannot find a specified Vulkan extension (Make sure required extensions are available).");
-		}
-		else
-		{
-			meedPlatformFPrint("Failed to create Vulkan instance (VkResult: %d).", result);
-		}
-
-		MEED_UNTOUCHABLE();
-	}
-#endif
 }
 
 static void deleteVulkanInstance(void* pData)
@@ -120,3 +115,79 @@ static void deleteVulkanInstance(void* pData)
 
 	vkDestroyInstance(g_vulkan->instance, MEED_NULL);
 }
+
+VKAPI_ATTR VkBool32 VKAPI_CALL vulkanDebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT	   messageSeverity,
+												   VkDebugUtilsMessageTypeFlagsEXT			   messageType,
+												   const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+												   void*									   pUserData)
+{
+	struct MEEDPlatformConsoleConfig config;
+
+	switch (messageSeverity)
+	{
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+		config.color = MEED_CONSOLE_COLOR_WHITE;
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+		config.color = MEED_CONSOLE_COLOR_GREEN;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+		config.color = MEED_CONSOLE_COLOR_YELLOW;
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+		config.color = MEED_CONSOLE_COLOR_RED;
+		break;
+	default:
+		config.color = MEED_CONSOLE_COLOR_RESET;
+	}
+
+	meedPlatformSetConsoleConfig(config);
+	meedPlatformFPrint("VULKAN VALIDATION LAYER: %s", pCallbackData->pMessage);
+
+	return VK_FALSE;
+}
+
+#if MEED_DEBUG
+static void deleteDebugMessenger(void*);
+
+static void createValidationLayers()
+{
+	MEED_ASSERT(g_vulkan != MEED_NULL);
+	MEED_ASSERT(g_vulkan->instance != MEED_NULL);
+	MEED_ASSERT(s_releaseStack != MEED_NULL);
+
+	VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo = {};
+
+	messengerCreateInfo.sType			= VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	messengerCreateInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+										  VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+										  VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	messengerCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+									  VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+									  VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	messengerCreateInfo.pfnUserCallback = vulkanDebugCallback;
+
+	PFN_vkCreateDebugUtilsMessengerEXT createDebugUtilsMessengerEXT =
+		(PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(g_vulkan->instance, "vkCreateDebugUtilsMessengerEXT");
+
+	MEED_ASSERT(createDebugUtilsMessengerEXT != MEED_NULL);
+	VK_ASSERT(
+		createDebugUtilsMessengerEXT(g_vulkan->instance, &messengerCreateInfo, MEED_NULL, &g_vulkan->debugMessenger));
+	meedReleaseStackPush(s_releaseStack, MEED_NULL, deleteDebugMessenger);
+}
+
+static void deleteDebugMessenger(void* pData)
+{
+	MEED_UNUSED(pData);
+
+	MEED_ASSERT(g_vulkan != MEED_NULL);
+	MEED_ASSERT(g_vulkan->instance != MEED_NULL);
+	MEED_ASSERT(g_vulkan->debugMessenger != MEED_NULL);
+
+	PFN_vkDestroyDebugUtilsMessengerEXT destroyDebugUtilsMessengerEXT =
+		(PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(g_vulkan->instance,
+																   "vkDestroyDebugUtilsMessengerEXT");
+
+	MEED_ASSERT(destroyDebugUtilsMessengerEXT != MEED_NULL);
+	destroyDebugUtilsMessengerEXT(g_vulkan->instance, g_vulkan->debugMessenger, MEED_NULL);
+}
+#endif
