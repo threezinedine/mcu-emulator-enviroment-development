c4e03f88cf5cdf0b4f956092365506af0e9a07c6
threezinedine <threezinedine@gmail.com>
2025-12-07 22:49:31 +0700

    [config] move to using manager lib by ntt

--- helper.py
+++ helper.py
@@ -1,26 +1,17 @@
+import os
 from config import (
-    Args,
     logger,
     SYSTEM,
     RunAutogen,
-    RunExample,
-    SetupLogging,
-    OpenDesigner,
-    BuildCProject,
-    RunPythonTest,
-    RunCProjectTest,
-    RunCApplication,
     CreateEnvironment,
     ValidateCommandExist,
     InstallCDependencies,
-    InstallPythonDependencies,
 )
+
+from ntt_project_manager import Manager
 
 
 def main():
-    args = Args()
-
-    SetupLogging(**args.Args)
 
     logger.debug("Start checking the system health...")
     ValidateCommandExist(SYSTEM.PythonCommand)
@@ -29,50 +20,52 @@ def main():
     ValidateCommandExist("git")
     logger.info("System health check passed.")
 
-    CreateEnvironment(dir="autogen", **args.Args)
-    InstallCDependencies()
-    RunAutogen(**args.Args)
+    Manager(baseDir=os.getcwd())
 
-    if args.IsBuild:
-        if args.IsCProject:
-            BuildCProject(**args.Args)
-        elif args.IsPythonProject:
-            pass
-    elif args.IsPackage:
-        if args.IsPythonProject:
-            InstallPythonDependencies(**args.Args)
-        elif args.IsCProject:
-            # InstallCDependencies(**args.Args)
-            pass
-        else:
-            raise NotImplementedError(
-                f'Packaging for project "{args.args.project}" is not implemented.'
-            )
-    elif args.IsRunTest:
-        if args.IsPythonProject:
-            RunPythonTest(**args.Args)
-        elif args.IsCProject:
-            BuildCProject(**args.Args)
-            RunCProjectTest(**args.Args)
-        else:
-            raise NotImplementedError(
-                f'Testing for project "{args.args.project}" is not implemented.'
-            )
-    elif args.IsRunDesigner:
-        OpenDesigner(**args.Args)
-    elif args.IsRun:
-        if args.IsPythonProject:
-            BuildCProject(**(args.Args | dict(project="engine")))
-        elif args.IsCProject:
-            RunCApplication(**args.Args)
-    elif args.IsRunExample:
-        RunExample(**args.Args)
-    elif args.IsRunAutogen:
-        pass
-    else:
-        raise NotImplementedError(
-            f"The command {args.args.command} is missing or not implemented."
-        )
+    CreateEnvironment(dir="autogen")
+    InstallCDependencies()
+    RunAutogen()
+
+    # if args.IsBuild:
+    #     if args.IsCProject:
+    #         BuildCProject(**args.Args)
+    #     elif args.IsPythonProject:
+    #         pass
+    # elif args.IsPackage:
+    #     if args.IsPythonProject:
+    #         InstallPythonDependencies(**args.Args)
+    #     elif args.IsCProject:
+    #         # InstallCDependencies(**args.Args)
+    #         pass
+    #     else:
+    #         raise NotImplementedError(
+    #             f'Packaging for project "{args.args.project}" is not implemented.'
+    #         )
+    # elif args.IsRunTest:
+    #     if args.IsPythonProject:
+    #         RunPythonTest(**args.Args)
+    #     elif args.IsCProject:
+    #         BuildCProject(**args.Args)
+    #         RunCProjectTest(**args.Args)
+    #     else:
+    #         raise NotImplementedError(
+    #             f'Testing for project "{args.args.project}" is not implemented.'
+    #         )
+    # elif args.IsRunDesigner:
+    #     OpenDesigner(**args.Args)
+    # elif args.IsRun:
+    #     if args.IsPythonProject:
+    #         BuildCProject(**(args.Args | dict(project="engine")))
+    #     elif args.IsCProject:
+    #         RunCApplication(**args.Args)
+    # elif args.IsRunExample:
+    #     RunExample(**args.Args)
+    # elif args.IsRunAutogen:
+    #     pass
+    # else:
+    #     raise NotImplementedError(
+    #         f"The command {args.args.command} is missing or not implemented."
+    #     )
 
 
 if __name__ == "__main__":
